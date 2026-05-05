79ab204a505ed486ff26a30d137c7b4a69f24bad
nxv2l <179728718+nxv2l@users.noreply.github.com>
2026-05-05 21:49:16 +0000

    Added install.sh #2

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-source ~/.bashrc
+# Only add if not already present
+if ! grep -q 'source ~/dotfiles/.bashrc' ~/.bashrc 2>/dev/null; then
+	echo 'source ~/dotfiles/.bashrc' >> ~/.bashrc
+fi
